6d16dbee4d03927bc430465770d7becee1938b03
Majd Saleh <majd.saleh95@gmail.com>
2023-04-12 23:19:08 -0700

    test connection without env file

--- server/db.js
+++ server/db.js
@@ -4,12 +4,20 @@ const fs = require('fs');
 const path = require('path');
 
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user:"postgres",
+  host: "18.188.125.26",
+  database: "reviewsDB",
+  password: "SDCPG95",
+  port: 5432,
 });
 
 
