c7d4d9dbbe626130e9235bcf141b22c3f6dec5fe
Majd Saleh <majd.saleh95@gmail.com>
2023-04-12 23:21:41 -0700

    hide db connection parameters in .env file

--- server/db.js
+++ server/db.js
@@ -4,21 +4,15 @@ const fs = require('fs');
 const path = require('path');
 
 
-// const pool = new Pool({
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-// });
-
 const pool = new Pool({
-  user:"postgres",
-  host: "18.188.125.26",
-  database: "reviewsDB",
-  password: "SDCPG95",
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
+
+
 
 
 module.exports = pool;
